f4bf0fb93c24d99d32cee3bf05d606ea9392d858
sc19hhl <sc19hhl@leeds.ac.uk>
2021-02-17 21:06:19 +0800

    change the admin page which displays the email, username, admin privileges instead of just email added a search field to allow the admins to search for a user based on their email and username added extra step if an admin wants to change their password changed password field to display a covered hashed password instead of the original password

--- advanced_booking/users/admin.py
+++ advanced_booking/users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from users.models import User
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
 
 
-admin.site.register(User)
+class UserAdminInterface(UserAdmin):
+    list_display = ('email', 'username', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, UserAdminInterface)
