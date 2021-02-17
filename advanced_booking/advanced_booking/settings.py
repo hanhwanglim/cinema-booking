ba9c46b458eb59811886f7d231b7845c8b489368
sc19hhl <sc19hhl@leeds.ac.uk>
2021-02-18 03:22:12 +0800

    Created the database for hall and seats but with bugs the relationship between seats and halls are not showing in the halls admin page

--- advanced_booking/advanced_booking/settings.py
+++ advanced_booking/advanced_booking/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'movies',
+    'halls',
 
     'django.contrib.admin',
     'django.contrib.auth',
--- advanced_booking/halls/models.py
+++ advanced_booking/halls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Hall(models.Model):
+    # TODO we probably dont want this to be null
+    name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+ 
+class Seat(models.Model):
+    SEAT_STATUS = [
+        ('X', 'Seat Taken'),
+        ('O', 'Seat Available'),
+        ('*', 'Seat Being Booked')
+    ]
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE, null=False)
+    row_number = models.PositiveSmallIntegerField()
+    seat_number = models.PositiveSmallIntegerField()
+    vip = models.BooleanField(default=False)
+    status = models.CharField(max_length=1, choices=SEAT_STATUS)
+
+    def __str__(self):
+        return f'Seat ({self.row_number}, {self.seat_number})'
