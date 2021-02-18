7772d48088046d8ae742e52ccf08f0f73f3f0ec6
sc19hhl <sc19hhl@leeds.ac.uk>
2021-02-19 02:41:11 +0800

    Created movie temporary movie routes the templates are just temporary placeholders for the frontend team to work on

--- advanced_booking/movies/views.py
+++ advanced_booking/movies/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Movie
+
+
+def index(request):
+    movie_list = Movie.objects.all()
+    context = {
+        'movie_list': movie_list,
+    }
+    return render(request, 'movies/index.html', context)
+
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/movie.html', context)
