31c78db40a94846c8be7eedc6860cf983b941f63
sc19hhl <sc19hhl@leeds.ac.uk>
2021-02-22 19:59:24 +0800

    created a simple test for login route

--- advanced_booking/users/tests.py
+++ advanced_booking/users/tests.py
@@ -1,3 +1,16 @@
+from datetime import date
+
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class LoginViewTest(TestCase):
+    def test_login_page(self):
+        """
+        The login page should have a form that allows users to login into.
+        """
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
