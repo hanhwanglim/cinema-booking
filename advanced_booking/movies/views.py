c30c49f55b1e98144e71451bb2b8d59cdff15998
Yaowen Shen <sc19ys@leeds.ac.uk>
2021-02-23 15:04:50 +0000

    fix SelectDatetimeForm and add it to html

--- advanced_booking/movies/views.py
+++ advanced_booking/movies/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from payment.forms import SelectDatetimeForm, TestForm
+from payment.forms import SelectDatetimeForm
 from .models import Movie
+from halls.models import Showtime
 
 
 def index(request):
@@ -19,17 +20,24 @@ def movie(request, movie_id):
     }
 
     if request.method == 'POST':
-        form = SelectDatetimeForm(request.POST
-                                  ,movie_id=1
-                                  )
+        form = SelectDatetimeForm(request.POST, movie_id=1)
+
         if form.is_valid():
             print("SelectDatetimeForm is valid")
-            return render("/")
+            selected_showtime = form.cleaned_data['selected_showtime']
+            # TODO: PASS showtime info to seat page.
+            # selected_showtime_id =
+            # return render("/")
         else:
             print(" Not a valid SelectDatetimeForm")
-
     else:
-        form=SelectDatetimeForm()
-    return render(request, 'movies/movie.html', context
-                  ,{'form': form}
-                  )
+        # create a Datatime form for the current page movie
+        form = SelectDatetimeForm(movie_id=1)
+        # add form to conext
+        context['form'] = form
+
+    return render(request, 'movies/movie.html', context)
+
+
+def seat(request, showtime_id):
+    pass
