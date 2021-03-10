f591731efc35a52e4dc387581a5ef4da2ac0c02c
sc19hhl <sc19hhl@leeds.ac.uk>
2021-03-11 02:49:57 +0800

    commented secrets.py imports

--- advanced_booking/advanced_booking/settings.py
+++ advanced_booking/advanced_booking/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from .secrets import EMAIL, PASSWORD
+# uncomment secrets.py file in future
+# from .secrets import EMAIL, PASSWORD
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = EMAIL
-EMAIL_HOST_PASSWORD = PASSWORD
+EMAIL_HOST_USER = 'EMAIL' # un-string EMAIL in future
+EMAIL_HOST_PASSWORD = 'PASSWORD' # un-string PASSWORD in future
