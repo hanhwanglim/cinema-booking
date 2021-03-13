28ca6dc4fcd7c5cd232219ca18b3996eee8097df
Imran Solim <ll14i2s@leeds.ac.uk>
2021-03-13 17:21:46 +0000

    combined imran and sullys work. login + homepage + registration

---
+++ advanced_booking/payment/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+
+from .models import ShoppingCart, Ticket, CardDetails, Order
+from movies.models import Movie
+from halls.models import Hall, Seat, Showtime
+
+
+# Register your models here.
+class TicketInterface(admin.ModelAdmin):
+    list_display = ('__str__', 'showtime', 'seat', 'age')
+    search_fields = ['age', 'seat', 'showtime']
+    readonly_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+class CardDetailsInterface(admin.ModelAdmin):
+    list_display = ('__str__', 'user', 'first4Digits', 'name')
+    search_fields = ['user', 'name']
+    readonly_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+class OrderInterface(admin.ModelAdmin):
+    list_display = ('__str__', 'user', 'Card', 'get_tickets', 'order_status', 'amount')
+    search_fields = ['user', 'tickets']
+    readonly_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    
+    # print all tickets in list_display
+    def get_tickets(self, obj):
+        return ", ".join([str(p) for p in obj.tickets.all()])
+
+
+class ShoppingCartInterface(admin.ModelAdmin):
+    list_display = ('__str__', 'user', 'ticket')
+    search_fields = ['user', 'tickets']
+    readonly_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Ticket, TicketInterface)
+admin.site.register(CardDetails, CardDetailsInterface)
+admin.site.register(Order, OrderInterface)
+admin.site.register(ShoppingCart)
---
+++ advanced_booking/payment/models.py
@@ -0,0 +1,74 @@
+#pip install django-credit-cards needed !!!!!!
+from django.db import models
+from movies.models import Movie
+from users.models import User
+from halls.models import Hall, Seat, Showtime
+from creditcards.models import CardNumberField, CardExpiryField, SecurityCodeField
+
+class CardDetails(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    number = CardNumberField('card number')
+    first4Digits = models.IntegerField(null=True)
+    name = models.CharField(max_length=100, null=True)
+    expiry = CardExpiryField('expiration date')
+
+    def __str__(self):
+        return str(self.number)
+
+#class CardDetails(models.Model):
+#    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+#    cardNumber: str = models.CharField(max_length=500, null=True)
+#    first4Digits = models.IntegerField(null=True)
+#    cardHolderFirstName = models.CharField(max_length=100, null=True)
+#    cardHolderLastName = models.CharField(max_length=100, null=True)
+#    ExpireYear = models.IntegerField(null=True, help_text="Please input 2 digits YY")
+#    ExpireMonth = models.IntegerField(null=True, help_text="Please input 2 digits MM")
+#    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+#    def __str__(self):
+#        return str(self.id)
+
+
+class Ticket(models.Model):
+    # ticket holder age, might apply discount when adding to cart
+    age = models.IntegerField(null=True)
+
+    seat = models.ForeignKey(Seat, null=True, on_delete=models.SET_NULL)
+
+    # can query time, hall and movie info from showtime
+    showtime = models.ForeignKey(Showtime, null=True, on_delete=models.SET_NULL)
+
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    ticket = models.ManyToManyField(Ticket)
+
+    def __str__(self):
+        return f'{self.user} - {self.ticket}'
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Unpaid', 'Unpaid'),
+        ('Pending', 'Pending'),
+        ('Succeed', 'Succeed'),
+    )
+
+    # one order for one user and a card
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    Card = models.ForeignKey(CardDetails, null=True, on_delete=models.SET_NULL)
+
+    # one order can have many tickets
+    tickets = models.ManyToManyField(Ticket)
+
+    order_status = models.CharField(max_length=200, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    amount = models.FloatField(null=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.order_status}'
